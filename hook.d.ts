0685b9f7173d01eecbd9d281014d0fa207c25796
Eloy Kim <eloy.kim@outlook.com>
2026-01-24 13:47:06 +0800

    Delete hook.d.ts

--- hook.d.ts
+++
@@ -1,4 +0,0 @@
-import { type BiPredicate, type DeepPropertyKey, type DeepPropertyValue } from "./utility";
-export declare let useCompare: <T>(t1: T, t2: T) => number;
-export declare let useRandom: <T = number | bigint>(index: T) => T;
-export declare let useTraverse: <T extends object>(t: T, callback: BiPredicate<DeepPropertyKey<T>, DeepPropertyValue<T>>) => void;
